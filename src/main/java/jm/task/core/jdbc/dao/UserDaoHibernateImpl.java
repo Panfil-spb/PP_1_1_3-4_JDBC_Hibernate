4466ba4f0075c00b9f4f2ba7f162d1c24bc7c038
Андрей <panfilovich1@gmail.com>
2023-11-14 21:05:05 +0300

    правки от ментора

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -115,9 +115,6 @@ public class UserDaoHibernateImpl implements UserDao {
             session.createSQLQuery("TRUNCATE TABLE users").executeUpdate();
             transaction.commit();
         } catch (Exception ignore) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
             System.out.println("cleanUsersTable: " + ignore.getMessage());
         }
     }
